8b43cca40b62ca331d8a8bf01a88fbe1425ebf87
Manav Misra <manavm@visionwebsoft.com>
2022-12-31 21:46:39 -0600

    feat:

--- app/index.js
+++ app/index.js
@@ -1,5 +1,17 @@
 import GradeController from "./grade/controller.js";
 
-GradeController.createStudent("John Doe").then((student) => {
-  console.log(student);
-});
+GradeController.createStudent("John Doe")
+  .then((student) => {
+    GradeController.addScore(student._id, {
+      type: "HW",
+      name: "HW 1",
+      earned: 15,
+      possible: 15,
+    });
+  })
+  .then(() => {
+    console.log("Done");
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
